cf19c9105853fd8f0c88173e048ba38ce32dbb0f
ReikoPaw <dev+github@dhelprave.com>
2025-02-18 02:19:55 +0100

    Cleanup code and comments

--- extractor.py
+++ extractor.py
@@ -6,14 +6,12 @@ parser = argparse.ArgumentParser()
 parser.add_argument("fileInput", nargs="?", default="./data.json")
 parser.add_argument("fileOutput", nargs="?", default="./output.txt")
 args = parser.parse_args()
-print(args)
-
 
 with open((args.fileInput), encoding="utf-8") as file:
     # Adds extracted URLs to url_list
     def tabs_from_window(json_window):
         for tab_counter in range(0, len(json_window["tabs"])):
-            # entries[0] due to json structure?
+            # ["entries"][0] due to json structure
             url_list.append(json_window["tabs"][tab_counter]["entries"][0]["url"])
 
     def append_text_to_file(file_path, _url_list):
@@ -25,15 +23,14 @@ with open((args.fileInput), encoding="utf-8") as file:
         except Exception as e:
             print(f"Error: {e}")
 
-    # loads json object
+    # Loads json object
     decoded_json_object = json.load(file)
 
-    # Preps an object to add urls to
+    # Preps an list to add urls to
     url_list = []
 
-    # Takes sends each indiviual window to extract tab URLs
+    # Loops each indiviual window to extract tab URLs
     for window_counter in range(0, len(decoded_json_object["windows"])):
-        # url_list.windows.append()
         tabs_from_window(decoded_json_object["windows"][window_counter])
 
     append_text_to_file((args.fileOutput), url_list)
